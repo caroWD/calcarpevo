b4888ad6d7e12650856efe986591c760fd75bbe6
caroDesign <juego.fuego@gmail.com>
2025-08-31 20:38:15 -0500

    refactor: add button type

--- src/components/molecules/Button.tsx
+++ src/components/molecules/Button.tsx
@@ -1,8 +1,8 @@
 import type { JSX } from 'react'
 
 type Props = {
-  type: 'icon' | 'text'
-  children: JSX.Element | string
+  type: 'icon' | 'text' | 'mix'
+  children: JSX.Element | string | [JSX.Element, string]
 }
 
 const Button = ({ type, children }: Props) => {
@@ -10,8 +10,10 @@ const Button = ({ type, children }: Props) => {
     <button className="size-[1.875rem] flex justify-center items-center rounded-full bg-indigo-200">
       {children}
     </button>
-  ) : (
+  ) : type === 'text' ? (
     <button className="py-6 px-3 text-white text-center text-xs bg-indigo-950">{children}</button>
+  ) : (
+    <button>{children}</button>
   )
 }
 
