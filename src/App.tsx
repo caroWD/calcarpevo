2e32d3be6a7cd71a38a1f485372609a485da054c
caroDesign <juego.fuego@gmail.com>
2025-09-01 00:28:51 -0500

    fix: general color text

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Routing from './router/Routing'
 
 function App() {
   return (
-    <div className="min-h-full flex flex-col justify-between gap-4">
+    <div className="min-h-full flex flex-col justify-between gap-4 text-white">
       <ThemeContextProvider>
         <Routing />
       </ThemeContextProvider>
