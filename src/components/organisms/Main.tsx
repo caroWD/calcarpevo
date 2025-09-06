48b1ec70e3d96ab2ed7af581ebfe7c198263b7b1
caroDesign <juego.fuego@gmail.com>
2025-09-05 20:37:40 -0500

    fix: overflow hidden

--- src/components/organisms/Main.tsx
+++ src/components/organisms/Main.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const Main = ({ children }: Props) => {
   return (
-    <main className="flex-1 flex flex-col *:flex-1 rounded-[1.6875rem] border border-slate-400 dark:border-slate-900 p-4 bg-linear-120 from-slate-200 dark:from-[#050D33] to-slate-300 dark:to-slate-950 shadow-md">
+    <main className="max-h-[calc(100dvh-(--spacing(44)))] grow flex flex-col *:flex-1 rounded-[1.6875rem] border border-slate-400 dark:border-slate-900 p-4 bg-linear-120 from-slate-200 dark:from-[#050D33] to-slate-300 dark:to-slate-950 shadow-md overflow-y-auto">
       {children}
     </main>
   )
